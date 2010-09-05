aa9bc985f38544547dad8f6163df2a234f64b3eb
electricbarfbag@googlemail.com <electricbarfbag@googlemail.com@e9dd444d-8b71-2fb6-74d7-835441985370>
2010-09-05 15:18:38 +0000

    Finished JavaDoc of HistoryObject.

--- trunk/CONE/src/edu/gullick/CONE/HistoryObject.java
+++ trunk/CONE/src/edu/gullick/CONE/HistoryObject.java
@@ -1,37 +1,71 @@
 package edu.gullick.CONE;
-
+/**
+ * The Class HistoryObject - a generic container for history objects - used for the undo feature.
+ */
 public class HistoryObject {
 
-	public static enum ACTION{
-		CENTER_PHYSICS,
-		EXPAND_NODE, 
-		CLOSE_NODE, 
-		ADD_WORD, 
-		DRAG_CANVAS, 
-		DRAG_NODE, 
-		DELETE_NODE, 
-		ADJUST_TFILTER, 
-		TOGGLE_DISPLAY_DEBUG, 
-		TOGGLE_DISPLAY_WORDS, 
-		TOGGLE_DISPLAY_EDGES, 
-		TOGGLE_DISPLAY_FORCES, 
-		TOGGLE_DISPLAY_NODES, 
-		TOGGLE_SMOOTH_FONT, 
-		TOGGLE_SMOOTH_ANIMATION, 
-		PAUSE_PHYSICS, 
-		SHOW_INDEX, 
-		ZOOM_IN, 
-		ZOOM_OUT
+	/**
+	 * The Enum ACTION - determines the type of action that needs to be undone.
+	 */
+	public static enum ACTION {
+		
+		/** The CENTE r_ physics. */
+		CENTER_PHYSICS, 
+		 /** The EXPAN d_ node. */
+		 EXPAND_NODE, 
+		 /** The CLOS e_ node. */
+		 CLOSE_NODE, 
+		 /** The AD d_ word. */
+		 ADD_WORD, 
+		 /** The DRA g_ canvas. */
+		 DRAG_CANVAS, 
+		 /** The DRA g_ node. */
+		 DRAG_NODE, 
+		 /** The DELET e_ node. */
+		 DELETE_NODE, 
+		 /** The ADJUS t_ tfilter. */
+		 ADJUST_TFILTER, 
+		 /** The TOGGL e_ displa y_ debug. */
+		 TOGGLE_DISPLAY_DEBUG, 
+		 /** The TOGGL e_ displa y_ words. */
+		 TOGGLE_DISPLAY_WORDS, 
+		 /** The TOGGL e_ displa y_ edges. */
+		 TOGGLE_DISPLAY_EDGES, 
+		 /** The TOGGL e_ displa y_ forces. */
+		 TOGGLE_DISPLAY_FORCES, 
+		 /** The TOGGL e_ displa y_ nodes. */
+		 TOGGLE_DISPLAY_NODES, 
+		 /** The TOGGL e_ smoot h_ font. */
+		 TOGGLE_SMOOTH_FONT, 
+		 /** The TOGGL e_ smoot h_ animation. */
+		 TOGGLE_SMOOTH_ANIMATION, 
+		 /** The PAUS e_ physics. */
+		 PAUSE_PHYSICS, 
+		 /** The SHO w_ index. */
+		 SHOW_INDEX, 
+		 /** The ZOO m_ in. */
+		 ZOOM_IN, 
+		 /** The ZOO m_ out. */
+		 ZOOM_OUT
 	};
-	
-	
+
+	/** The details of the onject (can be an object, a description ,etc). */
 	public Object details = null;
-	public HistoryObject.ACTION action;
 	
-	public HistoryObject( HistoryObject.ACTION action, Object details){
+	/** The action - type of history event. */
+	public HistoryObject.ACTION action;
+
+	
+	
+	/**
+	 * Instantiates a new history object.
+	 *
+	 * @param action the action
+	 * @param details the details
+	 */
+	public HistoryObject(HistoryObject.ACTION action, Object details) {
 		this.action = action;
 		this.details = details;
 	}
-	
-	
+
 }
