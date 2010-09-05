32445cb87739175ca60f481ff5f1eae680e69362
electricbarfbag@googlemail.com <electricbarfbag@googlemail.com@e9dd444d-8b71-2fb6-74d7-835441985370>
2010-09-05 14:00:43 +0000

    Finished javadoc of the WordAttraction class

--- trunk/CONE/src/edu/gullick/CONE/WordAttraction.java
+++ trunk/CONE/src/edu/gullick/CONE/WordAttraction.java
@@ -1,15 +1,27 @@
+/*
+ * 
+ */
 package edu.gullick.CONE;
 
 import edu.gullick.physics2D.Attraction;
 import edu.gullick.physics2D.Particle;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class WordAttraction simple extension of the Attraction class
+ * @extends edu.gullick.physics2D.Attraction
+ */
 public class WordAttraction extends Attraction {
 
-	public WordAttraction(Particle arg0, Particle arg1, float arg2, float arg3) {
-		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
+	/**
+	 * Instantiates a new word attraction.
+	 * @param particleA Particle A
+	 * @param particleB Particle B
+	 * @param strength float Strength
+	 * @param minDistance float minimum Distance
+	 */
+	public WordAttraction(Particle particleA, Particle particleB, float strength, float minDistance) {
+		super(particleA,particleB,strength,minDistance);
 	}
-
-	
 
 }
