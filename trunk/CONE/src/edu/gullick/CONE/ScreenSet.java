867c9647c0242bdc1d094e662cc748c6350dff83
electricbarfbag@googlemail.com <electricbarfbag@googlemail.com@e9dd444d-8b71-2fb6-74d7-835441985370>
2010-09-05 14:19:33 +0000

    Finished ScreenSet javadoc

--- trunk/CONE/src/edu/gullick/CONE/ScreenSet.java
+++ trunk/CONE/src/edu/gullick/CONE/ScreenSet.java
@@ -1,39 +1,89 @@
 package edu.gullick.CONE;
+/**
+ * The Class ScreenSet. Is used to record all of the settings of the screen at some time
+ */
 
 public class ScreenSet {
-	public int XOffset = 0;
-	public int YOffset = 0;
-	public double zoomLevel = 0;
 	
+	/** The X offset. */
+	public int XOffset = 0;
+	
+	/** The Y offset. */
+	public int YOffset = 0;
+	
+	/** The zoom level. */
+	public double zoomLevel = 0;
+
+	/**
+	 * Instantiates a new screen set.
+	 *
+	 * @param xOffset the x offset
+	 * @param yOffset the y offset
+	 * @param zoomLevel the zoom level
+	 */
 	public ScreenSet(int xOffset, int yOffset, double zoomLevel) {
 		XOffset = xOffset;
 		YOffset = yOffset;
 		this.zoomLevel = zoomLevel;
 	}
+	
+	
+	/* ***************************************
+	 * GETTERS AND SETTERS PAST HERE 
+	 ****************************************/
 
+	/**
+	 * Gets the x offset.
+	 *
+	 * @return the x offset
+	 */
 	public int getXOffset() {
 		return XOffset;
 	}
 
+	/**
+	 * Sets the x offset.
+	 *
+	 * @param xOffset the new x offset
+	 */
 	public void setXOffset(int xOffset) {
 		XOffset = xOffset;
 	}
 
+	/**
+	 * Gets the y offset.
+	 *
+	 * @return the y offset
+	 */
 	public int getYOffset() {
 		return YOffset;
 	}
 
+	/**
+	 * Sets the y offset.
+	 *
+	 * @param yOffset the new y offset
+	 */
 	public void setYOffset(int yOffset) {
 		YOffset = yOffset;
 	}
 
+	/**
+	 * Gets the zoom level.
+	 *
+	 * @return the zoom level
+	 */
 	public double getZoomLevel() {
 		return zoomLevel;
 	}
 
+	/**
+	 * Sets the zoom level.
+	 *
+	 * @param zoomLevel the new zoom level
+	 */
 	public void setZoomLevel(double zoomLevel) {
 		this.zoomLevel = zoomLevel;
 	}
-	
-	
+
 }
