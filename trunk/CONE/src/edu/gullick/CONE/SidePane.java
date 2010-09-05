a216ad28acac591e5fc90b283a642a3890cd2f3c
electricbarfbag@googlemail.com <electricbarfbag@googlemail.com@e9dd444d-8b71-2fb6-74d7-835441985370>
2010-09-05 14:04:14 +0000

    Finished SidePane JavaDoc

--- trunk/CONE/src/edu/gullick/CONE/SidePane.java
+++ trunk/CONE/src/edu/gullick/CONE/SidePane.java
@@ -1,73 +1,92 @@
 package edu.gullick.CONE;
+
 import javax.swing.*;
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
 
 
-public class SidePane{
+/**
+ * The Class SidePane - a small JFrame based window for looking up information without using a visualisation.
+ */
+public class SidePane {
 
 	/*
 	 * GLOBAL VARIABLES
 	 */
-	
-	
+
 	// the JFrame that everything is drawn on
+	/** The frame. */
 	public JFrame theFrame = new JFrame("CONE: Index");
-	
-	/*The JPanel that all of the components are contained within*/
+
+	/* The JPanel that all of the components are contained within */
+	/** The main panel. */
 	public JPanel mainPanel = new JPanel(new BorderLayout());
-	
-	/*the Controller - very very loose MVC*/
+
+	/* the Controller - very very loose MVC */
+	/** The controller - very loose MVC. */
 	public Controller theController = null;
-	
-	
-	/* Word Index for looking up words*/
+
+	/* Word Index for looking up words */
+	/** The word index. */
 	public WordIndex theWordIndex = null;
-	
+
+	/** The input field. */
 	public JTextField inputField = new JTextField(20);
-	
-	public JTextArea outputField = new JTextArea(20,20);
-	
+
+	/** The output field. */
+	public JTextArea outputField = new JTextArea(20, 20);
+
+	/** The search button. */
 	public JButton searchButton = new JButton("Go!");
 
 	
+	
+	
+	
+	/**
+	 * Instantiates a new side pane. Creates all of the components and adds them to the frame.
+	 *
+	 * @param theDriver the driver
+	 */
+	public SidePane(Controller theDriver) {
 
-	
-	
-	
-	
-	public SidePane( Controller theDriver){
-	
-		/*Setting up global variables*/
+		/* Setting up global variables */
 		this.theController = theDriver;
 
-		
-		/*Final frame configurations*/
+		/* Final frame configurations */
 		theFrame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
 		theFrame.setContentPane(mainPanel);
-		mainPanel.add(inputField,BorderLayout.NORTH);
-		mainPanel.add(searchButton,BorderLayout.SOUTH);
+		mainPanel.add(inputField, BorderLayout.NORTH);
+		mainPanel.add(searchButton, BorderLayout.SOUTH);
 		mainPanel.add(new JScrollPane(outputField));
-	
-		/*Setting up the progress bar*/
+
 		
+		/* setting up the button listener */
 		searchButton.addActionListener(theDriver);
 		theFrame.addWindowListener(theDriver);
-
-		/*Starting the screen, and making the frame visible*/
-		//theFrame.setVisible(true);
 		theFrame.pack();
 	}
+
 	
+	/* **************************************************
+	 * GETTERS AND SETTERS PAST HERE
+	 ***************************************************/
 	
-	public String getInputText(){
+	/**
+	 * Gets the input text.
+	 *
+	 * @return the input text
+	 */
+	public String getInputText() {
 		return inputField.getText();
 	}
-	
-	public void setOutputText(String s){
+
+	/**
+	 * Sets the output text.
+	 *
+	 * @param s the new output text
+	 */
+	public void setOutputText(String s) {
 		outputField.setText(s);
 	}
-	
-	
+
 }
