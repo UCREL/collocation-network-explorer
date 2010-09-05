4541858f4ab0622d8a794f10b3d124564ad100ef
electricbarfbag@googlemail.com <electricbarfbag@googlemail.com@e9dd444d-8b71-2fb6-74d7-835441985370>
2010-09-05 15:14:16 +0000

    Finished JavaDoc of LinkInformation Class

--- trunk/CONE/src/edu/gullick/CONE/LinkInformation.java
+++ trunk/CONE/src/edu/gullick/CONE/LinkInformation.java
@@ -1,67 +1,164 @@
 package edu.gullick.CONE;
 
+
+/**
+ * The Class LinkInformation - holds all of the information about a link between two words.
+ */
 public class LinkInformation {
+	
+	/** The word1. */
 	public String word1 = "";
+	
+	/** The word2. */
 	public String word2 = "";
+	
+	/** The affinity. */
 	public double affinity = 0.0D;
+	
+	/** The tscore. */
 	public double tscore = 0.0D;
+	
+	/** The frequency1. */
 	public double frequency1 = 0.0D;
+	
+	/** The frequency2. */
 	public double frequency2 = 0.0D;
 	
-	public LinkInformation(String word1,String  word2, double affinity, double tscore, Double frequency1, Double frequency2){
-		this. word1 = word1;
+	/** The frequency of both words together. */
+	public double frequencyBoth = 0.0D;
+
+	/**
+	 * Instantiates a new link information.
+	 *
+	 * @param word1 the word1
+	 * @param word2 the word2
+	 * @param affinity the affinity
+	 * @param tscore the tscore
+	 * @param frequency1 the frequency of Word1
+	 * @param frequency2 the frequency of Word2
+	 */
+	public LinkInformation(String word1, String word2, double affinity,
+			double tscore, Double frequency1, Double frequency2, Double frequencyBoth ) {
+		this.word1 = word1;
 		this.word2 = word2;
 		this.affinity = affinity;
 		this.tscore = tscore;
 		this.frequency1 = frequency1;
 		this.frequency2 = frequency2;
+		this.frequencyBoth = frequencyBoth;
 	}
-	
 
+	
+	
+	
+	/* ******************************
+	 * GETTERS & SETTERS PAST HERE!
+	 *******************************/
+	
+	/**
+	 * Gets the word1.
+	 *
+	 * @return the word1
+	 */
 	public String getWord1() {
 		return word1;
 	}
 
+	/**
+	 * Sets the word1.
+	 *
+	 * @param word1 the new word1
+	 */
 	public void setWord1(String word1) {
 		this.word1 = word1;
 	}
 
+	/**
+	 * Gets the word2.
+	 *
+	 * @return the word2
+	 */
 	public String getWord2() {
 		return word2;
 	}
 
+	/**
+	 * Sets the word2.
+	 *
+	 * @param word2 the new word2
+	 */
 	public void setWord2(String word2) {
 		this.word2 = word2;
 	}
 
+	/**
+	 * Gets the affinity.
+	 *
+	 * @return the affinity
+	 */
 	public double getAffinity() {
 		return affinity;
 	}
 
+	/**
+	 * Sets the affinity.
+	 *
+	 * @param affinity the new affinity
+	 */
 	public void setAffinity(double affinity) {
 		this.affinity = affinity;
 	}
 
+	/**
+	 * Gets the tscore.
+	 *
+	 * @return the tscore
+	 */
 	public double getTscore() {
 		return tscore;
 	}
 
+	/**
+	 * Sets the tscore.
+	 *
+	 * @param tscore the new tscore
+	 */
 	public void setTscore(double tscore) {
 		this.tscore = tscore;
 	}
 
+	/**
+	 * Gets the frequency1.
+	 *
+	 * @return the frequency1
+	 */
 	public double getFrequency1() {
 		return frequency1;
 	}
 
+	/**
+	 * Sets the frequency1.
+	 *
+	 * @param frequency1 the new frequency1
+	 */
 	public void setFrequency1(double frequency1) {
 		this.frequency1 = frequency1;
 	}
 
+	/**
+	 * Gets the frequency2.
+	 *
+	 * @return the frequency2
+	 */
 	public double getFrequency2() {
 		return frequency2;
 	}
 
+	/**
+	 * Sets the frequency2.
+	 *
+	 * @param frequency2 the new frequency2
+	 */
 	public void setFrequency2(double frequency2) {
 		this.frequency2 = frequency2;
 	}
