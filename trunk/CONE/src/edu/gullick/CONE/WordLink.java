adddd01691a31a1894a60588cebad8601e19b241
electricbarfbag@googlemail.com <electricbarfbag@googlemail.com@e9dd444d-8b71-2fb6-74d7-835441985370>
2010-09-05 13:37:49 +0000

    Finished javadoc of WordLink + removed some old code

--- trunk/CONE/src/edu/gullick/CONE/WordLink.java
+++ trunk/CONE/src/edu/gullick/CONE/WordLink.java
@@ -1,114 +1,103 @@
 package edu.gullick.CONE;
 
-/*
- * Class that acts as a Spring in the physics engine, but also as an edge in the visible graph
- */
-import java.awt.Color;
-
 import edu.gullick.physics2D.Spring;
 
+/**
+ * Class that acts as a Spring in the physics engine, but also as an edge in the visible graph
+ * @extends edu.gullick.physics2D.Spring
+ */
 
-public class WordLink extends Spring{
+public class WordLink extends Spring {
 
-	private Color color = null;
+	public enum DIRECTION {A_TO_B,B_TO_A,BIDIRECTIONAL}
+	
+	/** The thickness. */
 	private int thickness = 0;
-	private Double tscore = 0D;
+	
+	/** The is visible. */
 	private boolean isVisible = true;
-	
-	
-	/*
-	 * 0 for none
-	 * 1 for A --> B
-	 * 2 for B --> A
-	 * 3 for A <-> B
-	 */
+
+
+	/** The direction. */
 	private int direction = 0;
+
 	
-	
-	
-	
-	public WordLink(WordNode a, WordNode b, float length, float strength, float damping, Color color, int thickness, int direction, Double tscore) {
-		super(a, b, strength,damping, length);
+	/**
+	 * Instantiates a new word link.
+	 *
+	 * @param a the first WordNode (oneEnd)
+	 * @param b the second WordNode (theOtherEnd)
+	 * @param length the length
+	 * @param strength the strength
+	 * @param damping the damping
+	 * @param thickness the thickness
+	 * @param direction the direction
+	 */
+	public WordLink(WordNode a, WordNode b, float length, float strength, float damping, int thickness, int direction) {
+		super(a, b, strength, damping, length);
 		this.thickness = thickness;
 		this.direction = direction;
-		this.tscore = tscore;
 	}
 
+	
+	/*********************************
+	 *  GETTERS AND SETTERS BELOW THIS
+	 *********************************/
+	
 
-
-
-	public Color getColor() {
-		return color;
-	}
-
-
-
-
-	public void setColor(Color color) {
-		this.color = color;
-	}
-
-
-
-
+	/**
+	 * Gets the thickness.
+	 *
+	 * @return the thickness
+	 */
 	public int getThickness() {
 		return thickness;
 	}
 
-
-
-
+	/**
+	 * Sets the thickness.
+	 *
+	 * @param thickness the new thickness
+	 */
 	public void setThickness(int thickness) {
 		this.thickness = thickness;
 	}
 
-
-
-
+	/**
+	 * Gets the direction.
+	 *
+	 * @return the direction
+	 */
 	public int getDirection() {
 		return direction;
 	}
 
-
-
-
+	/**
+	 * Sets the direction.
+	 *
+	 * @param direction the new direction
+	 */
 	public void setDirection(int direction) {
 		this.direction = direction;
 	}
 
 
-
-
-	public void setTscore(Double tscore) {
-		tscore = tscore;
-	}
-
-
-
-
-	public Double getTscore() {
-		return tscore;
-	}
-
-
-
-
+	/**
+	 * Sets the visible.
+	 *
+	 * @param isVisible the new visible
+	 */
 	public void setVisible(boolean isVisible) {
 		this.isVisible = isVisible;
 	}
 
-
-
-
+	/**
+	 * Checks if is visible.
+	 *
+	 * @return true, if is visible
+	 */
 	public boolean isVisible() {
 		return isVisible;
 	}
-	
-	
-
-
-
-
-	
 
 }
